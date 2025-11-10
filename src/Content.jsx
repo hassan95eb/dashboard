242d86499b385f8ebf7fe4ca2cbb3eac85670c66
hassan95eb <19h.amini95@gmail.com>
2025-11-10 14:33:45 +0330

    create files

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Content() {
-  return <div className="w-full h-full bg-red-500">Content</div>;
+  return <div className="w-full h-full bg-red-100">Content</div>;
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,11 +5,10 @@ import Content from "./Content";
 export default function Dashboard() {
   return (
     <div className="w-full h-screen flex">
-      <div className="w-1/8 bg-blue-500">
+      <div className="w-48 bg-blue-500">
         <Sidebar />
       </div>
-
-      <div className="w-7/8">
+      <div className="w-full">
         <Content />
       </div>
     </div>
